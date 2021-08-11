9a1f4a1bc4fd4cfb85d6d841438dc57621a2f540
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-08-11 14:52:27 +0100

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,29 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\PostsController;
 use Illuminate\Support\Facades\Route;
 
+//User Routes
+Route::group(['namespace' => 'User'], function () {
 
-Route::get('/', function () {
-    return view('user.blog');
-});
-Route::get('/post', function () {
-    return view('user.post');
-})->name('post');
-Route::get('/admin/home', function () {
-    return view('admin.home');
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/post', [PostsController::class, 'index'])->name('post');
 });
 
 
-// Route::get('/admin/tag', function () {
-//     return view('admin.tag.tag');
+//Admin Routes
+
+Route::group(['namespace' => 'Admin'], function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::resource('admin/category', ' CategoryController');
+});
+
+
+// Route::get('/admin/home', function () {
+//     return view('admin.home');
 // });
-// Route::get('/admin/category', function () {
-//     return view('admin.category.category');
-// });
-Route::resource('admin/post', 'App\Http\Controllers\Admin\PostController');
